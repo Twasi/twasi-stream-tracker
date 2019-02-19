39fc037481f85b86ee3e4788a1d0db9d8a11a047
Merlin Westphal <email@merlinw.de>
2019-02-19 17:14:26 +0100

    + Query for track-entities by stream

--- src/main/java/net/twasiplugin/dependency/database/StreamTrackRepository.java
+++ src/main/java/net/twasiplugin/dependency/database/StreamTrackRepository.java
@@ -2,5 +2,17 @@ package net.twasiplugin.dependency.database;
 
 import net.twasi.core.database.lib.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamTrackRepository extends Repository<StreamTrackEntity> {
+
+    public List<StreamTrackEntity> getStreamEntitiesByStream(StreamEntity stream) {
+        try {
+            return store.createQuery(StreamTrackEntity.class).field("stream").equal(stream).asList();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+    }
+
 }
