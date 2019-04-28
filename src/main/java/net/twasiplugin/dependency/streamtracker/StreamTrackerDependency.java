c8f5a3975dfc29ee35a6d8bf09fb3ebcff417c30
Merlin Westphal <email@merlinw.de>
2019-04-28 20:47:24 +0200

    Stats for #Spendendose

--- src/main/java/net/twasiplugin/dependency/streamtracker/StreamTrackerDependency.java
+++ src/main/java/net/twasiplugin/dependency/streamtracker/StreamTrackerDependency.java
@@ -1,19 +1,26 @@
 package net.twasiplugin.dependency.streamtracker;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.google.gson.Gson;
 import net.twasi.core.database.models.User;
+import net.twasi.core.database.repositories.UserRepository;
 import net.twasi.core.events.NewInstanceEvent;
 import net.twasi.core.events.TwasiEventHandler;
 import net.twasi.core.plugin.TwasiDependency;
 import net.twasi.core.services.ServiceRegistry;
+import net.twasi.core.services.providers.DataService;
 import net.twasi.core.services.providers.InstanceManagerService;
 import net.twasi.twitchapi.helix.games.response.GameDTO;
 import net.twasi.twitchapi.options.TwitchRequestOptions;
+import net.twasiplugin.chatstatistics.database.ChatStatisticsEntity;
+import net.twasiplugin.chatstatistics.database.ChatStatisticsRepository;
 import net.twasiplugin.dependency.streamtracker.api.StreamResolver;
+import net.twasiplugin.dependency.streamtracker.database.StreamEntity;
+import net.twasiplugin.dependency.streamtracker.database.StreamRepository;
+import net.twasiplugin.dependency.streamtracker.database.StreamTrackRepository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static net.twasi.twitchapi.TwitchAPI.helix;
@@ -40,6 +47,49 @@ public class StreamTrackerDependency extends TwasiDependency {
                 tracker.start();
             }
         });
+
+        Thread t1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            UserRepository repo = ServiceRegistry.get(DataService.class).get(UserRepository.class);
+            StreamTrackRepository repo2 = ServiceRegistry.get(DataService.class).get(StreamTrackRepository.class);
+            ChatStatisticsRepository repo3 = ServiceRegistry.get(DataService.class).get(ChatStatisticsRepository.class);
+            User sd = repo.getByTwitchId("119965990");
+            List<StreamEntity> entities = ServiceRegistry.get(DataService.class).get(StreamRepository.class).getAllByUser(sd);
+            List<ChatStatisticsEntity> streamTrackEntities = new ArrayList<>();
+            entities.stream().map(repo3::getByStream).collect(Collectors.toList()).forEach(streamTrackEntities::addAll);
+            Map<String, Integer> emotes = new HashMap<>(), chatter = new HashMap<>();
+            streamTrackEntities.forEach(entitiy -> {
+                entitiy.getMessagesByUser().forEach((name, amount) -> {
+                    if (chatter.containsKey(name)) chatter.put(name, chatter.get(name) + amount);
+                    else chatter.put(name, amount);
+                });
+                entitiy.getUsedEmotes().forEach((emote, amount) -> {
+                    if (emotes.containsKey(emote)) emotes.put(emote, emotes.get(emote) + amount);
+                    else emotes.put(emote, amount);
+                });
+            });
+
+            Map<String, Integer> sChatter = chatter.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                    .limit(10)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                            (e1, e2) -> e1, LinkedHashMap::new)),
+
+                    sEmotes = emotes.entrySet().stream()
+                            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                            .limit(10)
+                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                                    (e1, e2) -> e1, LinkedHashMap::new));
+
+            System.out.println(new Gson().toJson(sChatter));
+            System.out.println(new Gson().toJson(sEmotes));
+        });
+        t1.setDaemon(true);
+        t1.start();
     }
 
     @Override
